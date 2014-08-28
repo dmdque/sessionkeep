6a725c71c21771b0d56313cd4b9588f2c8ed9379
Daniel Que <dmdque@gmail.com>
2014-08-28 17:54:34 -0400

    changed dev db

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/sessionkeep-dev'
+    //uri: 'mongodb://localhost/sessionkeep-dev'
+    uri: 'mongodb://daniel:daniel@kahana.mongohq.com:10078/sessionkeep'
   },
 
   seedDB: true
